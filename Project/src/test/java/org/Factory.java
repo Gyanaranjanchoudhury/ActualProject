f58b7af8da5a507068d125051fcc707b25dcc75c
GYANARANJAN <GYANARANJAN@DESKTOP-O0M08K0>
2025-04-01 09:27:43 +0530

    actual projet

---
+++ Project/src/test/java/org/Factory.java
@@ -0,0 +1,10 @@
+package org;
+
+public class Factory {
+
+	public static void main(String[] args) {
+		System.out.println("i have created ts");
+
+	}
+
+}
---
+++ Project/src/test/java/people/PhoneNumber.java
@@ -0,0 +1,9 @@
+package people;
+
+public class PhoneNumber {
+
+	public static void main(String[] args) {
+		System.out.println("phone number");
+	}
+
+}
